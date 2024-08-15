a5dd75ae4986489c765e6a0348d5e5757b019166
Nicholas Benedict <nicholasb1537@gmail.com>
2024-08-15 12:48:38 +0800

    add folke/zen-mode.nvim plugin

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,7 +5,7 @@
 local M = {}
 
 M.ui = {
-  theme = "gruvbox",
+  theme = "everforest",
 
   telescope = { style = "bordered" }, -- borderless / bordered
 
---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,11 @@
+return {
+  -- zen-mode
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+    },
+    lazy = false,
+  },
+}
